b6db2d2e67a247c0d7b63700c8634e834f349598
GokhanRepo <113057065+GokhanRepo@users.noreply.github.com>
2023-03-17 02:36:28 +0300

    update project Linux grid IP

--- src/test/java/com/apcrm/utilities/Driver.java
+++ src/test/java/com/apcrm/utilities/Driver.java
@@ -64,7 +64,7 @@ public class Driver {
                     break;
                 case "remote-chrome":
                     // assign your grid server address
-                    String gridAdress = "54.89.242.106"; // put your own Linux grid IP here
+                    String gridAdress = "54.160.46.104"; // put your own Linux grid IP here
                     try {
                         URL url = new URL("http://"+gridAdress+":4444/wd/hub");
                         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();

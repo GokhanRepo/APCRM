4748133624d5882f5bcfb348ac383c3a0d6d5504
GokhanRepo <113057065+GokhanRepo@users.noreply.github.com>
2023-03-21 20:37:04 +0300

    update runner

---
+++ src/test/java/com/apcrm/runners/CukesRunner.java
@@ -0,0 +1,23 @@
+package com.apcrm.runners;
+
+import io.cucumber.junit.*;
+import org.junit.runner.*;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                //"pretty",
+                "json:target/cucumber.json", // json reports
+                "html:target/cucumber-report.html", // html reports
+                "rerun:target/rerun.txt", // rerun failed scenarios
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber", // pretty reports
+        },
+        features = "src/test/resources/features", // path to features
+        glue = "apcrm/step_definitions", // path to step definitions
+        dryRun = false, // set to true to check if all steps have code
+        tags = "@hr",
+        publish = false // set to true to publish to the dashboard
+
+)
+public class CukesRunner {
+}

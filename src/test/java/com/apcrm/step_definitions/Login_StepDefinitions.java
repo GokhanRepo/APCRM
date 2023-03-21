9b01d9ca52967be43e203bc1b987b6f516209ecf
GokhanRepo <113057065+GokhanRepo@users.noreply.github.com>
2023-03-21 20:36:15 +0300

    update Login_StepDefinitions

--- src/test/java/com/apcrm/step_definitions/Login_StepDefinitions.java
+++ src/test/java/com/apcrm/step_definitions/Login_StepDefinitions.java
@@ -8,46 +8,26 @@ import io.cucumber.java.en.When;
 import com.apcrm.pages.LoginPage;
 
 public class Login_StepDefinitions {
+    LoginPage loginPage = new LoginPage();
 
     @Given("user is on the apcrm login page")
     public void user_is_on_the_apcrm_login_page() {
-        Driver.getDriver().get(ConfigurationReader.getProperty("web.url"));
-
+        Driver.getDriver().get(ConfigurationReader.getProperty("apcrmUrl"));
     }
 
     @When("user enters {string} username")
-    public void user_enter_username(String username) {
-        LoginPage.loginInput.sendKeys(username);
+    public void user_enters_username(String username) {
+        loginPage.loginInput.sendKeys(username);
     }
 
     @When("user enters {string} password")
     public void user_enters_password(String password) {
-
-        LoginPage.passwordInput.sendKeys(password);
+        loginPage.passwordInput.sendKeys(password);
     }
 
-    @Then("user should see the dashboard")
-    public void user_should_see_the_dashboard() {
-
-        LoginPage.loginButton.click();
+    @Then("user should see the activity stream page")
+    public void user_should_see_the_activity_stream_page() {
+        loginPage.loginButton.click();
     }
-
-
-
-
-
-
-//    @When("user enters {string} username")
-//    public void user_enters_username(String string) {
-//        // Write code here that turns the phrase above into concrete actions
-//        throw new io.cucumber.java.PendingException();
-//    }
-//
-//    @When("user enters {string} password")
-//    public void user_enters_password(String string) {
-//        // Write code here that turns the phrase above into concrete actions
-//        throw new io.cucumber.java.PendingException();
-//    }
-
 
 }

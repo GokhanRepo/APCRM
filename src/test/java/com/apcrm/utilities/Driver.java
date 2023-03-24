7f1ce17e9c96c320b248dd35a2768ea1ae754942
GokhanRepo <113057065+GokhanRepo@users.noreply.github.com>
2023-03-24 20:34:22 +0300

    update Driver.java

--- src/test/java/com/apcrm/utilities/Driver.java
+++ src/test/java/com/apcrm/utilities/Driver.java
@@ -3,6 +3,7 @@ package com.apcrm.utilities;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -51,6 +52,9 @@ public class Driver {
             */
             switch (browserType) {
                 case "chrome":
+
+                    ChromeOptions options = new ChromeOptions();
+                    options.addArguments("--remote-allow-origins=*");
 
                     WebDriverManager.chromedriver().setup();
                     driverPool.set(new ChromeDriver());

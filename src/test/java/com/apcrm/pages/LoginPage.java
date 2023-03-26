7f691cb8b7f89647325d3fe3b119c1bf98215ab9
GokhanRepo <113057065+GokhanRepo@users.noreply.github.com>
2023-03-26 16:48:46 +0300

    update LoginPage.java

--- src/test/java/com/apcrm/pages/LoginPage.java
+++ src/test/java/com/apcrm/pages/LoginPage.java
@@ -1,6 +1,5 @@
 package com.apcrm.pages;
 
-import com.apcrm.utilities.ConfigurationReader;
 import com.apcrm.utilities.Driver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -11,24 +10,23 @@ public class LoginPage {
         PageFactory.initElements(Driver.getDriver(), this);
     }
 
-    @FindBy(css ="input[name='USER_LOGIN']")
-    public  WebElement loginInput;
+    @FindBy(css = "input[name='USER_LOGIN']")
+    public WebElement loginInput;
     @FindBy(xpath = "//input[@name='USER_PASSWORD']")
-    public  WebElement passwordInput;
+    public WebElement passwordInput;
     @FindBy(css = "input[class='login-btn']")
-    public  WebElement loginButton;
+    public WebElement loginButton;
 
 
     /**
      * This method will log in using credentials from configuration.properties
      */
-
     public void loginWithConfiguration() {
         //String url = ConfigurationReader.getProperty("apcrmUrl");
-        Driver.getDriver().get(ConfigurationReader.getProperty("apcrmUrl"));
-        loginInput.sendKeys(ConfigurationReader.getProperty("loginUsername"));
-        passwordInput.sendKeys(ConfigurationReader.getProperty("loginPassword"));
-        loginButton.click();
+        //Driver.getDriver().get(ConfigurationReader.getProperty("apcrmUrl"));
+        //loginInput.sendKeys(ConfigurationReader.getProperty("loginUsername"));
+        //passwordInput.sendKeys(ConfigurationReader.getProperty("loginPassword"));
+        //loginButton.click();
     }
 
 }

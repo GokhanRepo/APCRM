d514268d46dd4fda711a4eb882ac8f4b5efbc49d
GokhanRepo <113057065+GokhanRepo@users.noreply.github.com>
2023-03-28 12:16:12 +0300

    update ActivityStreamPage

--- src/test/java/com/apcrm/pages/ActivityStreamPage.java
+++ src/test/java/com/apcrm/pages/ActivityStreamPage.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+import java.util.List;
+
 public class ActivityStreamPage {
 
 
@@ -19,9 +21,37 @@ public class ActivityStreamPage {
     @FindBy(css = "input[id='search-textbox-input']")
     public WebElement topPageSearchBox;
 
+    @FindBy (xpath = "//div[contains(@class, 'search-title-top-block')][.//div[@bx-search-item-id='P918']]")
+    public WebElement topPageSearchConversationsSection;
+
+    @FindBy (xpath = "//html/body/div[4]/div/div[2]/div[1]/div")
+    public WebElement topPageSearchConversationsText;
+
+    @FindBy (xpath = "/html/body/div[4]/div/div[1]/div[2]/div/div[1]/span")
+    public WebElement topPageSearchConversationsIcon;
+
+    @FindBy (xpath = "/html/body/div[4]/div/div[2]/div[2]/div/div[1]/a")
+    public WebElement topPageSearchConversationsLink;
+
+    @FindBy (partialLinkText = "blog")
+    public List<WebElement> topPageSearchBlogLinks;
+
     @FindBy(xpath = "//span[contains(@class, 'header-search-icon')]")
     public WebElement topPageSearchSubmitButton;
 
+
+
+    //  /html/body/div[4]/div/div[2]/div[2]/div/div[1]/a
+    // //*[contains(@class,'search-title-top-item search-title-top-item-js')]
+
+
+    // driver.findElement(By.partialLinkText("Plastic Spiral Bind")).click();
+    // JavascriptExecutor js = (JavascriptExecutor)driver;
+    // js.executeScript("arguments[0].click();", element);
+
+
+
+    // //div[@class='search-title-top-list search-title-top-list-js']/div[1]  first links in 4 sections of search (Employees, Conversations, Others, Search)
 
 }
 
